8a69752b737aa174f2423e5904c4d351f18b5d5b
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-17 13:13:24 +0000

    More fixes... part2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
-bash -lc "cd $PREFIX && gendef python${PY}.dll && dlltool -l Libs/libpython${py}.a -d python${PY}.def -k -A"
-bash -lc "export PATH=$PATH:$PREFIX/Library/bin && autoreconf -fi && ./configure --enable-python-integration --prefix=$PREFIX && make && make install"
+set TMP=%LOCALAPPDATA%\Temp
+set TEMP=%LOCALAPPDATA%\Temp
+bash -lc "cd $PREFIX && gendef python${CONDA_PY}.dll && dlltool -l Libs/libpython${CONDA_PY}.a -d python${CONDA_PY}.def -k -A"
+bash -lc "export PATH=$PATH:$PREFIX/Library/bin && autoreconf -fi && ./configure --enable-python-integration --prefix=`cygpath -u $PREFIX` PYTHON=`which python` && make && make install"
