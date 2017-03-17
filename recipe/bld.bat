c68615e0bbfdbaa16891ba3af10ecea5d7db1974
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-17 14:57:56 +0000

    More fixes... part3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
-set TMP=%LOCALAPPDATA%\Temp
-set TEMP=%LOCALAPPDATA%\Temp
+set TMP="%LOCALAPPDATA%\Temp"
+set TEMP="%LOCALAPPDATA%\Temp"
+set TMPDIR="%LOCALAPPDATA%\Temp"
 bash -lc "cd $PREFIX && gendef python${CONDA_PY}.dll && dlltool -l Libs/libpython${CONDA_PY}.a -d python${CONDA_PY}.def -k -A"
 bash -lc "export PATH=$PATH:$PREFIX/Library/bin && autoreconf -fi && ./configure --enable-python-integration --prefix=`cygpath -u $PREFIX` PYTHON=`which python` && make && make install"
