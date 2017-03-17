e630c7099da79a470a0f3930b34b9cbcda6abd34
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-03-17 10:03:56 +0000

    More fixes...

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,2 @@
 bash -lc "cd $PREFIX && gendef python${PY}.dll && dlltool -l Libs/libpython${py}.a -d python${PY}.def -k -A"
+bash -lc "export PATH=$PATH:$PREFIX/Library/bin && autoreconf -fi && ./configure --enable-python-integration --prefix=$PREFIX && make && make install"
