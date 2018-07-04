948225bb6d49d00d06aa9ed8738e7fd4a6c83cfe
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-07-04 11:26:31 +0100

    Windows: compile pcre from source

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,7 @@ set EXTRA_FLAGS=-DMS_WIN64
 set GCC_ARCH=i686-w64-mingw32
 )
 bash -lc "ln -s ${LOCALAPPDATA}/Temp /tmp"
+bash -lc "curl -L -O https://ftp.pcre.org/pub/pcre/pcre-8.42.tar.bz2 && tar xfj pcre-8.42.tar.bz2 && cd pcre-8.42 && ./configure --build=$GCC_ARCH --host=$GCC_ARCH --target=$GCC_ARCH --enable-utf8 --enable-pcre8 --prefix=/mingw-w64 && make && make install"
 bash -lc "curl -L -O https://downloads.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz && tar xfz swig-3.0.12.tar.gz && cd swig-3.0.12 && ./configure --build=$GCC_ARCH --host=$GCC_ARCH --target=$GCC_ARCH --without-perl5 --without-guile --disable-ccache --prefix=/mingw-w64 && make && make install"
 bash -lc "gendef ${PREFIX}/python${CONDA_PY}.dll && dlltool -l libpython${CONDA_PY}.a -d python${CONDA_PY}.def -k -A"
 bash -lc "autoreconf -fi && ./configure --disable-static --build=$GCC_ARCH --host=$GCC_ARCH --target=$GCC_ARCH --enable-python-integration --disable-fortran2003 --enable-python --prefix=`cygpath -u $PREFIX` --bindir=`cygpath -u $LIBRARY_BIN` --libdir=`cygpath -u $LIBRARY_LIB` PYTHON=`which python` SWIG=`which swig` CPPFLAGS=$EXTRA_FLAGS LIBS=-L$PWD"
@@ -27,4 +28,7 @@ bash -lc "rm `cygpath -u $SP_DIR`/xrayhelp.py*"
 bash -lc "rm `cygpath -u $SP_DIR`/xraymessages.py*"
 bash -lc "rm -r $PREFIX/share/xraylib"
 bash -lc "cd swig-3.0.12 && make uninstall"
+bash -lc "cd pcre-8.42 && make uninstall"
 bash -lc "rm -rf /mingw-w64/share/swig"
+bash -lc "rm -rf /mingw-w64/share/doc/pcre"
+bash -lc "rm -rf /mingw-w64/share/man/*/pcre*"
