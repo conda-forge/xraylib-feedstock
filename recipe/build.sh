0cfccc4db646bef37e5e26723a6a07ad41f7fc92
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-04-30 14:50:52 +0100

    3.99.0-test: DO NOT MERGE

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
+which bash
+autoreconf -i
 ./configure --enable-python --disable-perl --disable-java \
-	    --disable-fortran2003 --disable-lua --prefix=$PREFIX
+	    --disable-fortran2003 --disable-lua --prefix=$PREFIX SHELL=`which bash`
 make
-make check
 make install
