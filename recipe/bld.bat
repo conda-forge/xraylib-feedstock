7c5b7ed437b390a668aeabc695f9cc12fbec152b
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-05-29 07:35:54 +0100

    Test upcoming 4.1.0 release

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,37 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+setlocal EnableDelayedExpansion
+@echo on
+
+:: meson options
+set ^"MESON_OPTIONS=^
+  --prefix="%LIBRARY_PREFIX%" ^
+  --default-library=shared ^
+  --buildtype=release ^
+  --backend=ninja ^
+  -Dfortran-bindings=disabled ^
+  -Dpython-bindings=enabled ^
+  -Dpython-numpy-bindings=enabled ^
+  -Dpython=python ^
+ ^"
+
+:: configure build using meson
+meson setup builddir !MESON_OPTIONS!
 if errorlevel 1 exit 1
+
+:: print results of build configuration
+meson configure builddir
+if errorlevel 1 exit 1
+
+ninja -v -C builddir -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja -C builddir install -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+
+@REM For some reason conda-build decides that the meson files in Scripts are new?
+del %PREFIX%\Scripts\meson* %PREFIX%\Scripts\wraptool*
+
+@REM Meson doesn't put the Python files in the right place.
+cd %LIBRARY_PREFIX%\lib\python*
+cd site-packages
+move *xraylib* %PREFIX%\lib\site-packages\
